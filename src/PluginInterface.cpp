3064c0899ef2e27b9033a142120a83436911c8e7
Yuriy Dubinin <yuradubinin100@gmail.com>
2025-12-04 10:17:11 +0300

    Work in progress

--- src/PluginInterface.cpp
+++ src/PluginInterface.cpp
@@ -20,6 +20,16 @@ extern "C" void CreateReport(rapidjson::Value& request,
                              rapidjson::Value& response,
                              rapidjson::Document::AllocatorType& allocator,
                              CServerInterface* server) {
+    // Структура накопления итогов
+    struct Total {
+        double volume;
+        double commission;
+        double profit;
+        std::string currency;
+    };
+
+    std::unordered_map<std::string, Total> totals_map;
+
     std::string group_mask;
     int from;
     int to;
@@ -40,12 +50,46 @@ extern "C" void CreateReport(rapidjson::Value& request,
         server->GetCloseTradesByGroup(group_mask, from, to, &trades_vector);
         server->GetAllGroups(&groups_vector);
     } catch (const std::exception& e) {
-        std::cerr << "[DemoReportInterface]: " << e.what() << std::endl;
+        std::cerr << "[TradesHistoryReportInterface]: " << e.what() << std::endl;
     }
+
+    JSONObject id_column_props = {
+        {"name", JSONValue("ID")},
+        {"filter", JSONObject{{"type", JSONValue("search")}}}
+    };
+
+    JSONObject order_column_props = {
+        {"name", JSONValue("ORDER")},
+        {"filter", JSONObject{{"type", JSONValue("search")}}}
+    };
+
+    JSONArray table_data;
+    for (size_t i = 0; i < trades_vector.size(); i++) {
+        const auto& trade = trades_vector[i];
+
+        table_data.emplace_back(JSONObject{
+            {"id", JSONValue(std::to_string(i))},
+            {"order", JSONValue(std::to_string(trade.order))}
+        });
+    }
+
+    JSONObject table_props = props({
+        {"name", "MarginCallTable"},
+        {"idCol", "id"},
+        {"data", table_data},
+        {"orderBy", JSONArray{JSONValue("id"), JSONValue("DESC")}},
+        {"structure", JSONObject{
+            {"id", JSONValue(id_column_props)},
+            {"order", JSONValue(order_column_props)}
+        }}
+    });
+
+    Node table = Table({}, table_props);
 
     // Total report
     const Node report = div({
-        h1({text("Demo Report")})
+        h1({text("Trades History Report")}),
+        table
     });
 
     utils::CreateUI(report, response, allocator);
